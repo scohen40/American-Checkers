7bd75d8f4a8ec6c4b3588fcb6ea12ea32ae7be34
chavs1997 <vforer@student.touro.edu>
2019-01-10 16:53:55 -0500

    added possibleMovesForPlayer

--- American-Checkers/CheckersGame.cs
+++ American-Checkers/CheckersGame.cs
@@ -16,8 +16,152 @@ namespace AmericanCheckers
 
         }
 
+        public List<Board> PossibleBoardsForPlayer(Board InitialBoard)
+        {
+            List<Board> PotentialMoves = new List<Board>();
+            Player.Color OurColor = Game.UserPlayer.PlayerColor;
+            Cell.CellStatus OurStatus;
+            Cell.CellStatus OponentStatus;
+            if (OurColor.Equals(Player.Color.Red))
+            {
+                OurStatus = Cell.CellStatus.OccupiedRed;
+                OponentStatus = Cell.CellStatus.OccupiedBlack;
+            }
+            else
+            {
+                OurStatus = Cell.CellStatus.OccupiedBlack;
+                OponentStatus = Cell.CellStatus.OccupiedRed;
+            }
 
-        public List<Board> PossibleBoards(Board InitialBoard)
+
+            for (int row = 0; row < InitialBoard.BoardGame.Length; row++)
+            {
+                for (int col = 0; col < InitialBoard.BoardGame.Length; col++)
+                {
+                    Cell currentCell = InitialBoard.BoardGame[row, col];
+                    if (currentCell.Status.Equals(OurStatus))
+                    {
+                        if (currentCell.Queen)
+                        {
+                            if (row != InitialBoard.BoardGame.Length - 1)
+                            {
+                                if (col != InitialBoard.BoardGame.Length - 1)
+                                {
+                                    if (InitialBoard.BoardGame[row + 1, col + 1].Status.Equals(Cell.CellStatus.Unoccupied))
+                                    {
+                                        Board Potential = InitialBoard;
+                                        Potential.BoardGame[row, col] = new Cell(row, col, false, Cell.CellStatus.Unoccupied);
+                                        Potential.BoardGame[row + 1, col + 1] = new Cell(row + 1, col + 1, currentCell.Queen, OurStatus);
+                                        PotentialMoves.Add(Potential);
+                                    }
+                                    if (col != InitialBoard.BoardGame.Length - 2)
+                                    {
+                                        if (InitialBoard.BoardGame[row + 1, col + 1].Status.Equals(OponentStatus) &&
+                               InitialBoard.BoardGame[row + 2, col + 2].Status.Equals(Cell.CellStatus.Unoccupied))
+                                        {
+                                            Board Potential = InitialBoard;
+                                            Potential.BoardGame[row + 2, col + 2] = new Cell(row + 2, col + 2, currentCell.Queen, OurStatus);
+                                            Potential.BoardGame[row, col] = new Cell(row, col, false, Cell.CellStatus.Unoccupied);
+                                            Potential.BoardGame[row + 1, col + 1] = new Cell(row + 1, col + 1, false, Cell.CellStatus.Unoccupied);
+                                            PotentialMoves.Add(Potential);
+                                        }
+                                    }
+
+                                }
+                                if (col != 0)
+                                {
+                                    if (InitialBoard.BoardGame[row + 1, col - 1].Status.Equals(Cell.CellStatus.Unoccupied))
+                                    {
+                                        Board Potential = InitialBoard;
+                                        Potential.BoardGame[row, col] = new Cell(row, col, false, Cell.CellStatus.Unoccupied);
+                                        Potential.BoardGame[row + 1, col - 1] = new Cell(row + 1, col - 1, currentCell.Queen, OurStatus);
+                                        PotentialMoves.Add(Potential);
+                                    }
+                                    if (col != 1)
+                                    {
+                                        if (InitialBoard.BoardGame[row + 1, col - 1].Status.Equals(OponentStatus) &&
+                                   InitialBoard.BoardGame[row + 2, col - 2].Status.Equals(Cell.CellStatus.Unoccupied))
+                                        {
+                                            Board Potential = InitialBoard;
+                                            Potential.BoardGame[row + 2, col - 2] = new Cell(row + 2, col - 2, currentCell.Queen, OurStatus);
+                                            Potential.BoardGame[row, col] = new Cell(row, col, false, Cell.CellStatus.Unoccupied);
+                                            Potential.BoardGame[row + 1, col - 1] = new Cell(row + 1, col - 1, false, Cell.CellStatus.Unoccupied);
+                                            PotentialMoves.Add(Potential);
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+                        if (row != 0)
+                        {
+                            if (col != InitialBoard.BoardGame.Length - 1)
+                            {
+                                if (InitialBoard.BoardGame[row - 1, col + 1].Status.Equals(Cell.CellStatus.Unoccupied))
+                                {
+                                    Board Potential = InitialBoard;
+                                    Potential.BoardGame[row, col] = new Cell(row, col, false, Cell.CellStatus.Unoccupied);
+                                    Potential.BoardGame[row - 1, col + 1] = new Cell(row - 1, col + 1, true, OurStatus);
+                                    PotentialMoves.Add(Potential);
+                                }
+                                if (col != InitialBoard.BoardGame.Length - 2)
+                                {
+                                    if (InitialBoard.BoardGame[row - 1, col + 1].Status.Equals(OponentStatus) &&
+                            InitialBoard.BoardGame[row - 2, col + 2].Status.Equals(Cell.CellStatus.Unoccupied))
+                                    {
+                                        Board Potential = InitialBoard;
+                                        Potential.BoardGame[row - 2, col + 2] = new Cell(row - 2, col + 2, currentCell.Queen, OurStatus);
+                                        Potential.BoardGame[row, col] = new Cell(row, col, false, Cell.CellStatus.Unoccupied);
+                                        Potential.BoardGame[row - 1, col + 1] = new Cell(row - 1, col + 1, false, Cell.CellStatus.Unoccupied);
+                                        PotentialMoves.Add(Potential);
+                                    }
+                                }
+                            }
+                            if (col != 0)
+                            {
+                                if (InitialBoard.BoardGame[row - 1, col - 1].Status.Equals(Cell.CellStatus.Unoccupied))
+                                {
+                                    Board Potential = InitialBoard;
+                                    Potential.BoardGame[row, col] = new Cell(row, col, false, Cell.CellStatus.Unoccupied);
+                                    Potential.BoardGame[row - 1, col - 1] = new Cell(row - 1, col - 1, true, OurStatus);
+                                    PotentialMoves.Add(Potential);
+                                }
+
+                                if (col != 1)
+                                {
+                                    if (InitialBoard.BoardGame[row - 1, col - 1].Status.Equals(OponentStatus) &&
+                                        InitialBoard.BoardGame[row - 2, col - 2].Status.Equals(Cell.CellStatus.Unoccupied))
+                                    {
+                                        Board Potential = InitialBoard;
+                                        Potential.BoardGame[row - 2, col - 2] = new Cell(row - 2, col - 2, currentCell.Queen, OurStatus);
+                                        Potential.BoardGame[row, col] = new Cell(row, col, false, Cell.CellStatus.Unoccupied);
+                                        Potential.BoardGame[row - 1, col - 1] = new Cell(row - 1, col - 1, false, Cell.CellStatus.Unoccupied);
+                                        PotentialMoves.Add(Potential);
+                                    }
+                                }
+                            }
+
+                        }
+
+
+
+
+
+
+
+                    }
+                }
+            }
+
+
+
+            return PotentialMoves;
+        }
+
+
+
+
+        public List<Board> PossibleBoardsForComputer(Board InitialBoard)
         {
             List<Board> PotentialMoves = new List<Board>();
             Player.Color OurColor = Game.ComputerPlayer.PlayerColor;
